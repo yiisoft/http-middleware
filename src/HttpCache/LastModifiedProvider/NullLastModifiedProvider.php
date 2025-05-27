5af560bc867e444bc3708ee305e09a80f32c22f7
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-05-27 09:13:10 +0300

    Update src/HttpCache/LastModifiedProvider/NullLastModifiedProvider.php
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/HttpCache/LastModifiedProvider/NullLastModifiedProvider.php
+++ src/HttpCache/LastModifiedProvider/NullLastModifiedProvider.php
@@ -8,7 +8,7 @@ use DateTimeImmutable;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
- * Implementation of {@see LastModifiedProviderInterface} that returns null for all requests.
+ * Returns `null` for all requests.
  * It can be used when last modified functionality is not required.
  */
 final class NullLastModifiedProvider implements LastModifiedProviderInterface
